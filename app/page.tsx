8993a4f45a341f57157e864a899b6e74cebd533a
Ntare22 <jim.ntare@gmail.com>
2025-05-04 11:43:41 +0300

    setup nextjs complete

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Home() {
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+}
