1093c5b2fb608830f273795464f2460cc445733b
Ntare22 <jim.ntare@gmail.com>
2025-05-04 11:47:31 +0300

    setup fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
-import type { Config } from "tailwindcss"
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-
   content: [
     './pages/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
